a1d4175b4e1030d72db844dfc0479f4c82103326
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2023-02-01 20:49:16 -0300

    first commit

---
+++ src/test/java/com/dio/desafiospringboot/DesafioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.desafiospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
